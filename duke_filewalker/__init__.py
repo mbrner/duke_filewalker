c2786f686ce4c686f89e97b21fecc04f8b65a29e
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-02-16 11:48:53 +0100

    added 2 versions

--- duke_filewalker/__init__.py
+++ duke_filewalker/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from .extraction import Pattern, Extraction
 
@@ -14,8 +15,15 @@ class Walker:
             self.pattern = Pattern(pattern)
         self.top = os.path.dirname(self.pattern.split('<')[0])
         self.followlinks = followlinks
+        if sys.version_info >= (3,5):
+            self.walk = self.__walk_3_5__
+        else:
+            self.walk = self.__walk_legacy__
 
-    def walk(self, top=None):
+
+
+
+    def __walk_3_5__(self, top=None):
         pattern = self.pattern
         followlinks = self.followlinks
         if top is None:
@@ -32,6 +40,40 @@ class Walker:
                 new_pat = pattern.reduce_pattern(dir_entry.path)[0]
                 if new_pat is not None:
                     matching, extraction = new_pat.match(dir_entry.path,
+                                                         extract=True,
+                                                         sub=True)
+                    if matching:
+                        reduced_ext.append(extraction)
+                        if reduced_pat is None:
+                            reduced_pat = new_pat
+                        else:
+                            assert new_pat == reduced_pat
+        yield pattern, ext, reduced_pat, reduced_ext
+        if reduced_pat is None:
+            new_pathes = []
+        else:
+            new_pathes = [reduced_pat + ext_i for ext_i in reduced_ext]
+
+        for new_path in new_pathes:
+            if followlinks or not os.path.islink(new_path):
+                for x in self.walk(new_path):
+                    yield x
+
+    def __walk_legacy__(self, top=None):
+        pattern = self.pattern
+        followlinks = self.followlinks
+        ext = []
+        reduced_pat = None
+        reduced_ext = []
+        for name in os.listdir(top):
+            path = os.path.join(top, name)
+            matching, extraction = pattern.match(path, extract=True)
+            if matching:
+                ext.append(extraction)
+            elif os.path.isdir(path):
+                new_pat = pattern.reduce_pattern(path)[0]
+                if new_pat is not None:
+                    matching, extraction = new_pat.match(path,
                                                          extract=True,
                                                          sub=True)
                     if matching:
